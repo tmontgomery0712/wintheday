8dd048e062452703c2a7e8826d28a9f64050d201
Trevor Montgomery <trevormontgomery13@gmail.com>
2022-09-06 18:35:07 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'win-the-day-actual',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
